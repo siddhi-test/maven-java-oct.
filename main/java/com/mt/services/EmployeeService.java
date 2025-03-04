6665867d2ab47f53b30e2617e252b5db92dd1f8f
sanchitraj5 <87302682+sanchitraj5@users.noreply.github.com>
2025-03-04 20:53:43 +0530

    Update EmployeeService.java

--- main/java/com/mt/services/EmployeeService.java
+++ main/java/com/mt/services/EmployeeService.java
@@ -24,7 +24,7 @@ public class EmployeeService {
 		JSONObject js = new JSONObject();
 		js.put("Name", "DigiEras");
 		js.put("Calling Name", "Digi");
-		js.put("DOB", "08-Nov-2018");
+		js.put("DOB", "08-Nov-20189);
 		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
 		js.put("Places he like", "Goa");
 
